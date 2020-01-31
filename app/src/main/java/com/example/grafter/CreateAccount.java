1c3b6c0a5037199736f13aa881bdb7029417f40b
Duncan <duncanporritt@gmail.com>
2020-01-31 15:39:25 +0000

    Accounts interface implemented

--- app/src/main/java/com/example/grafter/CreateAccount.java
+++ app/src/main/java/com/example/grafter/CreateAccount.java
@@ -5,31 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class CreateAccount extends AppCompatActivity {
 
     // acc button -> new account
 
-    // NO usernames
-    // email address
-    // firstname
-    // lastname
-    // phone number
-    // password
-
-    // account type
-
     // address - ask after sign up?
     // payment info - req after sign up
 
-    private EditText Name;
-    private EditText Password;
+    private EditText Email;
     private EditText FirstName;
     private EditText LastName;
-    private EditText Email;
     private EditText Phone;
+    private EditText Password;
 
-    private Button Create;
+    private Spinner AccountType;
+
+    private Button SignUp;
 
 
     @Override
@@ -37,9 +30,15 @@ public class CreateAccount extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
-        Name = findViewById(R.id.textUsername);
-        Password = findViewById(R.id.textPassword);
-        Create = findViewById((R.id.createButton));
+        Email = findViewById(R.id.newEmailAddress);
+        FirstName = findViewById(R.id.newFirstName);
+        LastName = findViewById(R.id.newLastName);
+        Phone = findViewById(R.id.newPhoneNumber);
+        Password = findViewById(R.id.newPassword);
+
+        AccountType = findViewById(R.id.accountTypeSpinner);
+
+        SignUp = findViewById((R.id.createButton));
 
     }
 }
